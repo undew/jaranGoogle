1c07a49cce1f093bdf4609203ce9d2e33aa74a09
undew <08taka22s@gmail.com>
2020-08-26 18:40:46 +0900

    Initial Commit

--- weather.php
+++ weather.php
@@ -1,22 +1,22 @@
 <?php
 header("Content-type: application/xml; charset=UTF-8");
 if (empty($_GET)) {
-    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=leo16612c23f5b";
+    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=???????????";
 }
 // array_key_exists('reg',$_GET) = 'reg'キーが存在するかしないか 
 // reg = キー、$_GET = 配列
 
 else if (array_key_exists('reg', $_GET)) {
-    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=leo16612c23f5b&reg=" . $_GET['reg'];
+    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=&reg=" . $_GET['reg'];
 }   
 else if (array_key_exists('pref', $_GET)) {
-    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=leo16612c23f5b&pref=" . $_GET['pref'];
+    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=&pref=" . $_GET['pref'];
 }
 else if (array_key_exists('l_area', $_GET)) {
-    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=leo16612c23f5b&l_area=" . $_GET['l_area'];
+    $url =  "http://jws.jalan.net/APICommon/AreaSearch/V1/?key=&l_area=" . $_GET['l_area'];
 }
 else if (array_key_exists('s_area', $_GET)) {
-    $url =  "http://jws.jalan.net/APILite/HotelSearch/V1/?key=leo16612c23f5b&s_area=" . $_GET['s_area'];
+    $url =  "http://jws.jalan.net/APILite/HotelSearch/V1/?key=&s_area=" . $_GET['s_area'];
 }
 $xml = file_get_contents($url);
 echo $xml;
